1fe2404439ee8df863ebfe2a896b3e78aa757ada
anadoh <68846949+anadoh@users.noreply.github.com>
2021-03-15 20:52:37 +0100

    figure

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
-import Figure from './components/Figure';
-import WrongLetters from './components/WrongLetters';
-import Word from './components/Word';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import "./App.css";
 
-const words = ['police', 'ananas', 'carrot', 'bike', 'sport', 'application'];
+const words = ["police", "ananas", "carrot", "bike", "sport", "application"];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
-  const [playable, setPlayable] = useState(true)
+  const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
 
-//--------------------------------
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key;
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+          } else {
+            //showNotification();
+          }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+              setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+            } else {
+              //showNotification();
+            }
+          }
+        }
+    };
+    window.addEventListener("keydown", handleKeyDown);
 
-window.addEventListener("keydown", (e) => {
-  //console.log(e.keyCode);
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
-    const letter = e.key;
-    if (selectedWord.includes(letter)) {
-      if (!correctLetters.includes(letter)) {
-        correctLetters.push(letter);
-        displayWord();
-      } else {
-        showNotification();
-      }
-    } else {
-      if (!wrongLetters.includes(letter)) {
-        wrongLetters.push(letter);
-
-        updateWrongLettersElement();
-      } else {
-        showNotification();
-      }
-    }
-  }
-});
-// ------------------------------
-
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [correctLetters, wrongLetters, playable]);
 
   return (
     <>
-      <Header/>
-      <div className='game-container'>
-        <Figure/>
-        <WrongLetters/>
-        <Word selectedWord={selectedWord} correctLetters={correctLetters}/>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
     </>
   );
--- src/components/Figure.js
+++ src/components/Figure.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Figure = () => {
+const Figure = ({wrongLetters}) => {
+    const errors = wrongLetters.length;
     return (
-        <svg height="250" width="200" class="figure-container">
+        <svg height="250" width="200" className="figure-container">
             <line x1="60" y1="20" x2="140" y2="20" />
             <line x1="140" y1="20" x2="140" y2="60" />
             <line x1="60" y1="20" x2="60" y2="220" />
             <line x1="20" y1="220" x2="100" y2="220" />
-            <circle class="figure-part" cx="140" cy="80" r="20" />
-            <line class="figure-part" x1="140" y1="100" x2="140" y2="150" />
-            <line class="figure-part" x1="140" y1="100" x2="120" y2="130" />
-            <line class="figure-part" x1="140" y1="100" x2="160" y2="130" />
-            <line class="figure-part" x1="140" y1="150" x2="130" y2="190" />
-            <line class="figure-part" x1="140" y1="150" x2="150" y2="190" />
+            {errors > 0 && <circle  cx="140" cy="80" r="20" />}
+            {errors > 1 && <line  x1="140" y1="100" x2="140" y2="150" />}
+            {errors > 2 && <line  x1="140" y1="100" x2="120" y2="130" />}
+            {errors > 3 && <line  x1="140" y1="100" x2="160" y2="130" />}
+            {errors > 4 && <line  x1="140" y1="150" x2="130" y2="190" />}
+            {errors > 5 && <line  x1="140" y1="150" x2="150" y2="190" />}
         </svg>
     )
 }
